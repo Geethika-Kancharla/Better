14bfe3e54c4217aef7551e67afbdb4a76ef552cd
Geethika-Kancharla <geethikak004@gmail.com>
2024-08-04 13:21:11 +0530

    Footer

---
+++ app/components/Footer.js
@@ -0,0 +1,110 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-white text-black h-screen flex flex-col lg:pl-24'>
+            <h3 className='p-4 text-green-900 font-bold text-2xl'>Better</h3>
+            <div className='flex flex-col lg:flex lg:flex-row lg:p-4'>
+                <div className='pl-4 flex flex-col bg-white lg:w-1/3'>
+                    <div className=''>
+
+                        <p className='p-4 text-slate-700'>Better is a family of companies serving all your homeownership needs.</p>
+                    </div>
+                    <div>
+                        <div className='flex space-x-2'>
+                            <h3 className='pl-4 text-green-900 font-bold text-2xl'>Better</h3>
+                            <h3 className='text-slate-500 text-2xl'>Mortage</h3>
+                        </div>
+                        <p className='p-4 text-slate-700'>We can’t wait to say “Welcome home.” Apply 100% online, with expert customer support.</p>
+                    </div>
+
+                    <div>
+                        <div className='flex space-x-2'>
+                            <h3 className='pl-4 text-green-900 font-bold text-2xl'>Better</h3>
+                            <h3 className='text-slate-500 text-2xl'>Real Estate</h3>
+                        </div>
+                        <p className='p-4 text-slate-700'>Connect with a local Better Real Estate Partner Agent to find out all the ways you can save.</p>
+                    </div>
+
+                    <div>
+                        <div className='flex space-x-2'>
+                            <h3 className='pl-4 text-green-900 font-bold text-2xl'>Better</h3>
+                            <h3 className='text-slate-500 text-2xl'>Cover</h3>
+                        </div>
+                        <p className='p-4 text-slate-700'>Shop, bundle, and save on insurance coverage for home, auto, life, and more.</p>
+                    </div>
+
+                    <div>
+                        <div className='flex space-x-2'>
+                            <h3 className='pl-4 text-green-900 font-bold text-2xl'>Better</h3>
+                            <h3 className='text-slate-500 text-2xl'>Inspect</h3>
+                        </div>
+                        <p className='p-4 text-slate-700'> Get free repair estimates, 24-hour turnarounds on reports, and rest easy with our 100-day inspection guarantee.</p>
+                    </div>
+
+                    <div>
+                        <div className='flex space-x-2  '>
+                            <h3 className='pl-4 text-green-900 font-bold text-2xl'>Better</h3>
+                            <h3 className='text-slate-500 text-2xl'>Settlement Services</h3>
+                        </div>
+                        <p className='p-4 text-slate-700'>Get transparent rates when you shop for title insurance all in one convenient place.</p>
+                    </div>
+
+                </div>
+
+                <div className='flex flex-col space-y-3 lg:pl-10'>
+                    <h5 className='pl-4 font-bold'>Resources</h5>
+                    <p className='text-slate-700 pl-4'>Home affordability calculator</p>
+                    <p className='text-slate-700 pl-4'>Mortgage calculator</p>
+                    <p className='text-slate-700 pl-4'>Free mortgage calculator</p>
+                    <p className='text-slate-700 pl-4'>Mortgage calculator with taxes</p>
+                    <p className='text-slate-700 pl-4'>Mortgage calculator with PMI</p>
+                    <p className='text-slate-700 pl-4'>Rent vs buy calculator</p>
+                    <p className='text-slate-700 pl-4'>HELOC payment calculator</p>
+                    <p className='text-slate-700 pl-4'>HELOC vs cash-out refinance calculator</p>
+                    <p className='text-slate-700 pl-4'>Buy a home</p>
+                    <p className='text-slate-700 pl-4'>Sell a home</p>
+                    <p className='text-slate-700 pl-4'>Get home inspection</p>
+                </div>
+
+                <div className='flex flex-col pt-8 space-y-3 lg:pt-0 lg:pl-10'>
+                    <h5 className='pl-4 font-bold'>Company</h5>
+                    <p className='text-slate-700 pl-4'>About Us</p>
+                    <p className='text-slate-700 pl-4'>Careers</p>
+                    <p className='text-slate-700 pl-4'>Media</p>
+                    <p className='text-slate-700 pl-4'>Partner With Us</p>
+                    <p className='text-slate-700 pl-4'>Learning center</p>
+                    <p className='text-slate-700 pl-4'>FAQs</p>
+                    <p className='text-slate-700 pl-4'>Investor Relations</p>
+                </div>
+
+                <div className='lg:pt-0'>
+
+                    <div className='flex flex-col pt-8 space-y-3 lg:pt-0 lg:pl-10'>
+                        <h5 className='pl-4 font-bold'>Contact Us</h5>
+                        <p className='text-slate-700 pl-4'>hello@better.com</p>
+                        <p className='text-slate-700 pl-4'>415-523-8837</p>
+                        <p className='text-slate-700 pl-4'>FAQ</p>
+                        <p className='text-slate-700 pl-4'>Glossary</p>
+                    </div>
+
+                    <div className='flex flex-col pt-8 space-y-3 lg:pl-10'>
+                        <h5 className='pl-4 font-bold'>Legal</h5>
+                        <p className='text-slate-700 pl-4'>NMLS Consumer Access</p>
+                        <p className='text-slate-700 pl-4'>Privacy Policy</p>
+                        <p className='text-slate-700 pl-4'>Terms of Use</p>
+                        <p className='text-slate-700 pl-4'>Disclosures & Licensing</p>
+                        <p className='text-slate-700 pl-4'>Affiliated Business</p>
+                        <p className='text-slate-700 pl-4'>Browser Disclaimer</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Footer
