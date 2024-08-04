efc55ced70f672080e7efa024b28da3e4f2bfbee
Geethika-Kancharla <geethikak004@gmail.com>
2024-08-04 18:04:40 +0530

    Completed Nav

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,9 +1,228 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react';
+import { FaPhone } from "react-icons/fa";
+import { LuAlignJustify } from "react-icons/lu";
+import { IoMdClose } from "react-icons/io";
+import { BiChevronDown } from 'react-icons/bi';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <div className="flex flex-col">
+            <nav className="flex justify-between p-2 bg-[#004733]">
+                <div className="flex space-x-3 p-2">
+                    <p className="text-white text-3xl">Better</p>
+                    <div className="hidden lg:flex space-x-3">
+
+                        <div className="relative lg:pt-2">
+                            {/* Main Navigation Links */}
+                            <div className="hidden lg:flex space-x-3">
+                                <div className="relative group">
+                                    <a
+                                        href="#"
+                                        className="text-white lg:p-4 rounded-3xl hover:bg-white hover:text-green-900 lg:text-xl"
+                                    >
+                                        Buy
+
+                                    </a>
+                                    {/* Dropdown Menu */}
+                                    <div className="absolute left-0 mt-6 hidden group-hover:block bg-white text-black rounded-lg shadow-lg w-80 space-y-3">
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Apply Now</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Purchase Rates</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Affordability calculator</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Mortgage calculator</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Rent vs buy calculator</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Find an agent</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">VA loans</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Learning center</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative lg:pt-2">
+                            {/* Main Navigation Links */}
+                            <div className="hidden lg:flex space-x-3">
+                                <div className="relative group">
+                                    <a
+                                        href="#"
+                                        className="text-white lg:p-4 rounded-3xl hover:bg-white hover:text-green-900 lg:text-xl"
+                                    >
+                                        Refinance
+
+                                    </a>
+                                    {/* Dropdown Menu */}
+                                    <div className="absolute left-0 mt-6 hidden group-hover:block bg-white text-black rounded-lg shadow-lg w-80 space-y-3">
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Apply Now</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Refinance Rates</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Cash-out refinance calculator</a>
+
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Learning center</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="relative lg:pt-2">
+                            {/* Main Navigation Links */}
+                            <div className="hidden lg:flex space-x-3">
+                                <div className="relative group">
+                                    <a
+                                        href="#"
+                                        className="text-white lg:p-4 rounded-3xl hover:bg-white hover:text-green-900 lg:text-xl"
+                                    >
+                                        HELOC
+
+                                    </a>
+                                    {/* Dropdown Menu */}
+                                    <div className="absolute left-0 mt-6 hidden group-hover:block bg-white text-black rounded-lg shadow-lg w-80 space-y-3">
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Apply Now</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Calculate your Cash</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">HELOC vs. Cash-out-Refinance</a>
+
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Learning center</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="relative lg:pt-2">
+                            {/* Main Navigation Links */}
+                            <div className="hidden lg:flex space-x-3">
+                                <div className="relative group">
+                                    <a
+                                        href="#"
+                                        className="text-white lg:p-4 rounded-3xl hover:bg-white hover:text-green-900 lg:text-xl"
+                                    >
+                                        Rates
+
+                                    </a>
+                                    {/* Dropdown Menu */}
+                                    <div className="absolute left-0 mt-6 hidden group-hover:block bg-white text-black rounded-lg shadow-lg w-80 space-y-3">
+
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Purchase Mortage Rates</a>
+
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Refinance rates</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Refinance cash-out-rates</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">HELOC rates</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Purchase VA rates</a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="relative lg:pt-2">
+                            {/* Main Navigation Links */}
+                            <div className="hidden lg:flex space-x-3">
+                                <div className="relative group">
+                                    <a
+                                        href="#"
+                                        className="text-white lg:p-4 rounded-3xl hover:bg-white hover:text-green-900 lg:text-xl"
+                                    >
+                                        Better+
+
+                                    </a>
+                                    {/* Dropdown Menu */}
+                                    <div className="absolute left-0 mt-6 hidden group-hover:block bg-white text-black rounded-lg shadow-lg w-80 space-y-3">
+
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Get Insurance</a>
+
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Title and Closing</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Better Attorney Match</a>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-100">Learning Center</a>
+                                        <div className='flex space-x-3'>
+                                            <a href="#" className="block px-4 py-2 hover:bg-gray-100">Better Agent Match</a>
+                                            <button className='bg-yellow-500 rounded-3xl px-3 text-sm'>For Agents</button>
+                                        </div>
+                                        <div className='flex space-x-3 pb-4'>
+                                            <a href="#" className="block px-4 py-2 hover:bg-gray-100">Better Duo</a>
+                                            <button className='bg-yellow-500 rounded-3xl px-3 text-sm'>For Agents</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+                <div className="flex p-2 space-x-3 lg:space-x-10">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full border border-white">
+                        <FaPhone className="text-white text-2xl" />
+                    </div>
+                    <p className='hidden lg:flex lg:py-2'>Sign in</p>
+                    <button onClick={toggleMenu} className="p-2 lg:hidden text-white text-3xl">
+                        {menuOpen ? <IoMdClose /> : <LuAlignJustify />}
+                    </button>
+                </div>
+            </nav>
+            {menuOpen && (
+                <div className="fixed h-screen inset-0  bg-white flex flex-col">
+                    <div className="flex justify-between items-center p-4">
+                        <div className="text-black text-3xl font-bold">Better</div>
+                        <button onClick={toggleMenu} className="text-black text-3xl mb-4">
+                            <IoMdClose />
+                        </button>
+                    </div>
+                    <div className="flex flex-col p-4 flex-grow space-y-4">
+
+                        <div className="flex justify-between items-center py-2">
+                            <a href="#" className="text-black text-xl">Buy</a>
+                            <BiChevronDown className="text-black text-xl" />
+                        </div>
+                        <div className="flex justify-between items-center py-2">
+                            <a href="#" className="text-black text-xl">Refinance</a>
+                            <BiChevronDown className="text-black text-xl" />
+                        </div>
+                        <div className="flex justify-between items-center py-2">
+                            <a href="#" className="text-black text-xl">HELOC</a>
+                            <BiChevronDown className="text-black text-xl" />
+                        </div>
+                        <div className="flex justify-between items-center py-2">
+                            <a href="#" className="text-black text-xl">Rates</a>
+                            <BiChevronDown className="text-black text-xl" />
+                        </div>
+                        <div className="flex justify-between items-center py-2">
+                            <a href="#" className="text-black text-xl">Better+</a>
+                            <BiChevronDown className="text-black text-xl" />
+                        </div>
+                        <div className="flex items-center justify-center space-x-2 w-full bg-green-100 rounded-3xl p-2 text-black">
+                            <FaPhone className="text-xl" />
+                            <button className="text-center">Call us anytime at (415) 523 8837</button>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col p-4 space-y-2">
+
+                        <div className="flex flex-col items-center justify-center space-x-2 w-full bg-green-300 rounded-3xl p-2 text-black">
+                            <p>Get started</p>
+                            <p>3min | No credit impact</p>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center space-x-2 w-full rounded-3xl p-2 text-black border border-green-200">
+                            <p>Sign in</p>
+
+                        </div>
+
+
+                    </div>
+                </div>
+            )}
+
+
+        </div>
+    );
 }
-
-export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React, { useState } from 'react';
 import Footer from "./components/Footer";
-import { FaPhone } from "react-icons/fa";
-import { LuAlignJustify } from "react-icons/lu";
-import { IoMdClose } from "react-icons/io";
-import { BiChevronDown } from 'react-icons/bi';
+
+import Navbar from './components/Navbar'
 
 export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -15,80 +13,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col">
-      <nav className="flex justify-between p-2 bg-[#004733]">
-        <div className="flex space-x-3 p-2">
-          <p className="text-white text-3xl">Better</p>
-          <div className="hidden lg:flex space-x-3">
-            <a href="#" className="text-white lg:px-3 lg:py-2 rounded-3xl hover:bg-white hover:text-green-900">Buy</a>
-            <a href="#" className="text-white lg:px-3 lg:py-2 rounded-3xl hover:bg-white hover:text-green-900">Refinance</a>
-            <a href="#" className="text-white lg:px-3 lg:py-2 rounded-3xl hover:bg-white hover:text-green-900">HELOC</a>
-            <a href="#" className="text-white lg:px-3 lg:py-2 rounded-3xl hover:bg-white hover:text-green-900">Rates</a>
-            <a href="#" className="text-white lg:px-3 lg:py-2 rounded-3xl hover:bg-white hover:text-green-900">Better+</a>
-          </div>
-        </div>
-        <div className="flex p-2 space-x-3 lg:space-x-10">
-          <div className="flex items-center justify-center w-12 h-12 rounded-full border border-white">
-            <FaPhone className="text-white text-2xl" />
-          </div>
-          <p className='hidden lg:flex lg:py-2'>Sign in</p>
-          <button onClick={toggleMenu} className="p-2 lg:hidden text-white text-3xl">
-            {menuOpen ? <IoMdClose /> : <LuAlignJustify />}
-          </button>
-        </div>
-      </nav>
-      {menuOpen && (
-        <div className="fixed h-screen inset-0  bg-white flex flex-col">
-          <div className="flex justify-between items-center p-4">
-            <div className="text-black text-3xl font-bold">Better</div>
-            <button onClick={toggleMenu} className="text-black text-3xl mb-4">
-              <IoMdClose />
-            </button>
-          </div>
-          <div className="flex flex-col p-4 flex-grow space-y-4">
-
-            <div className="flex justify-between items-center py-2">
-              <a href="#" className="text-black text-xl">Buy</a>
-              <BiChevronDown className="text-black text-xl" />
-            </div>
-            <div className="flex justify-between items-center py-2">
-              <a href="#" className="text-black text-xl">Refinance</a>
-              <BiChevronDown className="text-black text-xl" />
-            </div>
-            <div className="flex justify-between items-center py-2">
-              <a href="#" className="text-black text-xl">HELOC</a>
-              <BiChevronDown className="text-black text-xl" />
-            </div>
-            <div className="flex justify-between items-center py-2">
-              <a href="#" className="text-black text-xl">Rates</a>
-              <BiChevronDown className="text-black text-xl" />
-            </div>
-            <div className="flex justify-between items-center py-2">
-              <a href="#" className="text-black text-xl">Better+</a>
-              <BiChevronDown className="text-black text-xl" />
-            </div>
-            <div className="flex items-center justify-center space-x-2 w-full bg-green-100 rounded-3xl p-2 text-black">
-              <FaPhone className="text-xl" />
-              <button className="text-center">Call us anytime at (415) 523 8837</button>
-            </div>
-          </div>
-
-          <div className="flex flex-col p-4 space-y-2">
-
-            <div className="flex flex-col items-center justify-center space-x-2 w-full bg-green-300 rounded-3xl p-2 text-black">
-              <p>Get started</p>
-              <p>3min | No credit impact</p>
-            </div>
-
-            <div className="flex flex-col items-center justify-center space-x-2 w-full rounded-3xl p-2 text-black border border-green-200">
-              <p>Sign in</p>
-
-            </div>
-
-
-          </div>
-        </div>
-      )}
-
+      <Navbar />
       <Footer />
     </div>
   );
